933f24e11334bb71fb0d2ae9d851c201c636be15
Andrei Boronin <77534166+Andrew-dev-cmd@users.noreply.github.com>
2025-05-06 02:09:52 +0300

    add error tests

--- tests/routers/location/test_add_location.py
+++ tests/routers/location/test_add_location.py
@@ -1,4 +1,4 @@
-from core.database import Base, get_db
+from core.database import get_db
 from main import app
 from fastapi.testclient import TestClient
 from tests.main import override_get_db
@@ -23,3 +23,19 @@ def test_add_location(_):
     locations = client.get("/locations", headers={"Authorization": f"Bearer {token}"})
     assert isinstance(locations.json(), list)
     assert len(locations.json()) == 1
+
+def test_add_location_not_authenticated(_):
+    response = client.post("/locations?location=Moscow")
+    assert response.status_code == 403
+    assert response.json()["detail"] == "Not authenticated"
+
+
+def test_add_location_missing_field(_):
+    user_data = {"username": "test_user", "email": "test_user@test.example", "password": "testpassword"}
+    client.post("/auth/register", json=user_data)
+
+    user = client.post("/auth/login", json={"username": "test_user", "password": "testpassword"})
+    token = user.json()["access_token"]
+    
+    response = client.post("/locations", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 422
