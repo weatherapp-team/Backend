3e40fe47fb011873662b87cef573113fcf33d830
Andrei Boronin <77534166+Andrew-dev-cmd@users.noreply.github.com>
2025-05-06 01:56:59 +0300

    Add test location save

---
+++ tests/routers/location/test_add_location.py
@@ -0,0 +1,25 @@
+from core.database import Base, get_db
+from main import app
+from fastapi.testclient import TestClient
+from ...main import override_get_db
+from ...main import test_db as _
+
+app.dependency_overrides[get_db] = override_get_db
+
+client = TestClient(app)
+
+
+def test_add_location(_):
+    user_data = {"username": "test_user", "email": "test_user@test.example", "password": "testpassword"}
+    client.post("/auth/register", json=user_data)
+
+    user = client.post("/auth/login", json={"username": "test_user", "password": "testpassword"})
+    token = user.json()["access_token"]
+    
+    response = client.post("/locations?location=Moscow", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 201
+    assert response.json()["message"] == "Location saved successfully"
+
+    locations = client.get("/locations", headers={"Authorization": f"Bearer {token}"})
+    assert isinstance(locations.json(), list)
+    assert len(locations.json()) == 1
